ddb07e7a561ad214eb21631b70b067a97ce105be
Danang Ponorogo <danang.dandies@gmail.com>
2021-06-29 23:16:38 +0700

    first commit

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,25 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UsersController {
+  public async index({ inertia }: HttpContextContract) {
+    return inertia.render('Test', { nama: 'Danang Ponorogo' })
+  }
+
+  public async create({ }: HttpContextContract) {
+  }
+
+  public async store({ }: HttpContextContract) {
+  }
+
+  public async show({ }: HttpContextContract) {
+  }
+
+  public async edit({ }: HttpContextContract) {
+  }
+
+  public async update({ }: HttpContextContract) {
+  }
+
+  public async destroy({ }: HttpContextContract) {
+  }
+}
---
+++ resources/js/Pages/Test.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let nama = "Danang";
+</script>
+
+<h1>Halo {nama}</h1>
+
+<style>
+  h1 {
+    color: chartreuse;
+  }
+</style>
